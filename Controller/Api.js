0ac8a22739e12898609e04dfce88d4d9d29f416f
Supraja-Kurapati <kurapatisupraja8@gmail.com>
2024-02-02 17:30:52 +0530

    deploy

--- Controller/Api.js
+++ Controller/Api.js
@@ -1,38 +1,5 @@
-// const bcrypt=require('bcryptjs')
-// const secretkey="E-cart"
-
-// const jwt=require('jsonwebtoken')
-// const saltRound=10;
-
-// const UserCollection=require('../Model/UserModel')
-// const register= async (req,res)=>{
-//     try{
-//     data=req.body
-//     console.log(data);
-//     // data.password=bcrypt.hashSync(data.password,saltRound)
-//    const account=await UserCollection.findOne({email:data.email})
-
-//    if(account){
-//     // return res.send({msg:"Email Already Exist"})
-//     return res.status(400).send({msg:"Email Already Exists"})
-//    }
-//    data.password=bcrypt.hashSync(data.password,saltRound)
-
-//    const insertData=await UserCollection.create(data)
-
-//    const token=jwt.sign({user:data.email},secretkey)
-//    res.send({msg:"user Registered Successfully",token:token,insertData:insertData})
-// }
-// catch(err){
-// console.error(err,"Error during registration")
-// res.status(500).send({msg:"Registration failed"})
-// }
-// }
-
-// module.exports={register}
 
 //atlas
-
 const bcrypt=require('bcryptjs')
 const secretkey="7ken"
 const jwt=require('jsonwebtoken')
@@ -59,8 +26,8 @@ const login=async(req,res)=>{
     console.log(data);
     const acc=await newCollection.findOne({email:data.email})
     if(acc){
-        // const login=bcrypt.compareSync(data.password,acc.password)
-        const login=bcrypt.compareSync(data.email,acc.email)
+         const login=bcrypt.compareSync(data.password,acc.password)
+      //  const login=bcrypt.compareSync(data.email,acc.email)
 
     
     if(login){
