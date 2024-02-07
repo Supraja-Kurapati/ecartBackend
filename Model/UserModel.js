cfae37948368edee429ddf9aafbc79dce7fff581
Supraja-Kurapati <kurapatisupraja8@gmail.com>
2024-02-07 15:46:40 +0530

    new

--- Model/UserModel.js
+++ Model/UserModel.js
@@ -19,3 +19,33 @@
 // const UserCollection=mongoose.model('User',userSchema)
 
 // module.exports=UserCollection
+const mongoose = require('mongoose'); 
+
+// Declare the Schema of the Mongo model
+var userSchema = new mongoose.Schema({
+    firstname:{
+        type:String,
+        required:true,
+        unique:true,
+        index:true,
+    },
+        lastname:{
+        type:String,
+        required:true,
+        unique:true,
+        index:true,
+    },
+
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+});
+
+//Export the model
+module.exports = mongoose.model('User', userSchema);
